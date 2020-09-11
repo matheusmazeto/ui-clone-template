c6e3eae3bf737b6d7674bdce87f72b7ca4339b1f
Matheus Mazeto <mgmazeto@gmail.com>
2020-09-11 18:34:06 -0300

    :art: format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,42 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
   extends: [
-    "plugin:react/recommended",
-    "standard",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "prettier/standard",
-    "prettier/react",
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/standard',
+    'prettier/react'
   ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module'
   },
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "space-before-function-paren": ["error", "never"],
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'prettier/prettier': 'error',
+    'space-before-function-paren': ['error', 'never'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-use-before-define': [0],
+    '@typescript-eslint/no-use-before-define': [1]
   },
   settings: {
-    "import/resolver": {
-      typescript: {},
+    'import/resolver': {
+      typescript: {}
     },
     react: {
-      version: "detect",
-    },
-  },
-};
+      version: 'detect'
+    }
+  }
+}
