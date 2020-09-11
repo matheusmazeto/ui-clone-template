e764980f4bcec6a99be9e0f540ce572dc676a87c
Matheus Mazeto <mgmazeto@gmail.com>
2020-09-11 18:30:51 -0300

    :wrench: add .eslint.js

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "prettier/standard",
+    "prettier/react",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "space-before-function-paren": ["error", "never"],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+    react: {
+      version: "detect",
+    },
+  },
+};
