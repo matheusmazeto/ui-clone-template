41cd947cde278b53dd2e25a09b70b7b3a4b3bbb6
Matheus Mazeto <mgmazeto@gmail.com>
2020-09-12 15:18:08 -0300

    :sparkles: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
+import GlobalStyles from 'styles/global'
+
 const App = () => {
-  return <div>UI Clone Template</div>
+  return (
+    <>
+      <div>UI Clone Template</div>
+      <GlobalStyles />
+    </>
+  )
 }
 
 export default App
